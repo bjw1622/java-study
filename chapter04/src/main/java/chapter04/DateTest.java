17e93e472e77e3fd52f2dbe12b152713f60b7661
백재원 <bjw1622@gmail.com>
2023-08-16 09:23:01 +0900

    feat : class명 수정

--- chapter04/src/main/java/chapter04/DateTest.java
+++ chapter04/src/main/java/chapter04/DateTest.java
@@ -3,7 +3,7 @@ package chapter04;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class DateTEst {
+public class DateTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
